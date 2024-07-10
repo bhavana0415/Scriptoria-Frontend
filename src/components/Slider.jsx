850dc10c081ff8cd0d254f55a078c093be9293f2
BHAVANA MANASWINI <bhavanamanaswi@gmail.com>
2024-07-10 17:34:38 +0530

    Slider changes

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -10,63 +10,14 @@ import CelebrationIcon from '@mui/icons-material/Celebration';
 
 const images = [
     "https://www.gutenberg.org/cache/epub/29024/pg29024.cover.medium.jpg",
-    "https://www.gutenberg.org/cache/epub/2701/pg2701.cover.medium.jpg",
-    "https://www.gutenberg.org/cache/epub/145/pg145.cover.medium.jpg",
-    "https://www.gutenberg.org/cache/epub/2641/pg2641.cover.medium.jpg",
-    "https://www.gutenberg.org/cache/epub/100/pg100.cover.medium.jpg",
+    // "https://www.gutenberg.org/cache/epub/2701/pg2701.cover.medium.jpg",
+    // "https://www.gutenberg.org/cache/epub/145/pg145.cover.medium.jpg",
+    // "https://www.gutenberg.org/cache/epub/2641/pg2641.cover.medium.jpg",
+    // "https://www.gutenberg.org/cache/epub/100/pg100.cover.medium.jpg",
     "https://www.gutenberg.org/cache/epub/1342/pg1342.cover.medium.jpg",
-    "https://www.gutenberg.org/cache/epub/16389/pg16389.cover.medium.jpg",
+    // "https://www.gutenberg.org/cache/epub/16389/pg16389.cover.medium.jpg",
     "https://www.gutenberg.org/cache/epub/37106/pg37106.cover.medium.jpg",
 ]
-// const slides = [
-//     {
-//         bgColor: "bg-gradient-to-r from-purple-900 to-indigo-800",
-//         title: "Welcome to Book Haven",
-//         heading: "Unleash Your Imagination",
-//         content: "Dive into a world of endless possibilities with our extensive collection of eBooks. From thrilling adventures to heartwarming romances, find your next favorite story today.",
-//         image: "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
-//     },
-//     {
-//         bgColor: "bg-gradient-to-r from-green-900 to-blue-800",
-//         title: "Discover Hidden Gems",
-//         heading: "Where New Authors Shine",
-//         content: "Explore a curated selection of debut novels and hidden literary treasures. Support emerging writers and discover fresh, original content.",
-//         image: "https://wallpapercave.com/wp/wp3386769.jpg",
-//     },
-//     {
-//         bgColor: "bg-gradient-to-r from-pink-900 to-fuchsia-800",
-//         title: "Author Success Stories",
-//         heading: "Your Journey to Bestseller",
-//         content: "Learn from successful authors who started just like you. Get inspired by their stories and embark on your path to becoming a best-selling novelist.",
-//         image: "https://wallpaperaccess.com/full/809523.jpg",
-//     },
-//     {
-//         bgColor: "bg-gradient-to-r from-cyan-900 to-teal-800",
-//         title: "How It Works",
-//         heading: "Write, Publish, Thrive",
-//         content: "Join our community of writers. Create, publish, and share your stories with readers around the world. Our platform makes it easy for you to bring your stories to life.",
-//         image: "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
-//     }
-// ];
-
-const slides = [
-    {
-        bgColor: "bg-gradient-to-r from-purple-200 to-indigo-300",
-        title: "About Us",
-    },
-    {
-        bgColor: "bg-gradient-to-r from-green-200 to-blue-300",
-        title: "Discover",
-    },
-    {
-        bgColor: "bg-gradient-to-r from-pink-200 to-fuchsia-300",
-        title: "Author Success Stories",
-    },
-    {
-        bgColor: "bg-gradient-to-r from-cyan-200 to-teal-300",
-        title: "How It Works",
-    }
-];
 
 export default function Slider() {
     const [current, setCurrent] = useState(0);
@@ -75,19 +26,19 @@ export default function Slider() {
     const previousSlide = () => {
         setCurrent((prev) => {
             if (prev == 0) {
-                return slides.length - 1
+                return 3
             }
-            return (prev - 1) % slides.length
+            return (prev - 1) % 4
         }
         );
     };
 
     const nextSlide = () => {
         setCurrent((prev) => {
-            if (prev == slides.length - 1) {
+            if (prev == 3) {
                 return 0;
             }
-            return (prev + 1) % slides.length
+            return (prev + 1) % 4
         }
         );
     };
@@ -123,13 +74,13 @@ export default function Slider() {
                     </div>
                 </div>
                 <div className={`min-w-full h-screen bg-gradient-to-r from-pink-${currentMode == 'dark' ? 900 : 200} to-fuchsia-${currentMode == 'dark' ? 800 : 300} flex items-center justify-center`}>
-                    <div className="w-full px-10">
+                    <div className="w-full px-10 flex flex-col md:flex-row items-center">
                         <div className="w-full md:w-3/5">
-                            <div className="grid gap-1 sm:grid-cols-2 lg:grid-cols-4">
+                            <div className="grid gap-1 sm:grid-cols-1 lg:grid-cols-3">
                                 {images.map((img, ind) => (
-                                    <div key={ind} className="rounded-xl shadow-lg transform transition duration-500 hover:scale-105">
+                                    <div key={ind} className={`rounded-xl transform transition duration-500 hover:scale-105 mt-${ind * 4} `}>
                                         <a href="#">
-                                            <div className="relative flex items-center justify-center overflow-hidden rounded-xl bg-fuchsia-300" style={{ height: '250px' }}>
+                                            <div className="relative flex items-center justify-center overflow-hidden rounded-xl bg-fuchsia-300 h-80">
                                                 <img className="object-fill w-full h-full" src={img} alt="Book Cover" />
                                             </div>
                                         </a>
@@ -137,27 +88,34 @@ export default function Slider() {
                                 ))}
                             </div>
                         </div>
-                        <div className="w-full">
-                            
+                        <div className="w-full md:w-2/5 p-10">
+                            <div className="justify-center items-center text-center">
+                                <p className="text-lg">Whether you're looking for your next great read or just browsing, our selection of books has something for everyone. Discover your new favorite book today!</p>
+                                <a href="/all-books" className="font-mono mt-4 inline-block px-6 py-3 bg-rose-400 text-stone-900 font-semibold rounded-lg shadow-md hover:bg-rose-500 transition hover:translate-y-1 hover:scale-105 duration-300">Browse Collection</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className={`min-w-full h-screen bg-gradient-to-r from-cyan-${currentMode == 'dark' ? 900 : 200} to-teal-${currentMode == 'dark' ? 800 : 300} flex items-center justify-center`}>
                     <div className="w-full max-w-screen-lg mx-auto p-8 text-white">
                         <div className="flex flex-col md:flex-row items-center">
-                            <div className="w-full md:w-1/2">
-                                <img src={slides[3].image} className="object-cover w-full h-full" alt={slides[3].title} />
+                            <div className="w-full md:w-1/2 flex flex-col items-center text-center">
+                                <span className="rounded-full bg-cyan-500 p-3 text-5xl text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="currentColor" fillRule="evenodd" d="M7.16 3.5C4.73 5.06 3.55 6.67 3.55 9.36c.16-.05.3-.05.44-.05c1.27 0 2.5.86 2.5 2.41c0 1.61-1.03 2.61-2.5 2.61c-1.9 0-2.99-1.52-2.99-4.25c0-3.8 1.75-6.53 5.02-8.42L7.16 3.5zm7 0c-2.43 1.56-3.61 3.17-3.61 5.86c.16-.05.3-.05.44-.05c1.27 0 2.5.86 2.5 2.41c0 1.61-1.03 2.61-2.5 2.61c-1.89 0-2.98-1.52-2.98-4.25c0-3.8 1.75-6.53 5.02-8.42l1.14 1.84h-.01z" clipRule="evenodd" /></svg>
+                                </span>
+
+                                <blockquote className="mt-8 flex-1">
+                                    <p className="max-w-xl text-lg font-medium leading-relaxed text-foreground">Using insights from the story analytics dashboard and reader feedback, S.S. Sahoo completely rewrote her book  seven times before it was published. It has since earned over 70 million reads.</p>
+                                </blockquote>
                             </div>
-                            <div className="w-full md:w-1/2 px-4">
-                                <h2 className="text-3xl font-bold">{slides[3].title}</h2>
-                                <h3 className="text-xl">{slides[3].heading}</h3>
-                                <p>{slides[3].content}</p>
+                            <div className="w-full md:w-1/2">
+                                <img className="object-contain" src="src/assets/author.png" alt="" />
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className={`min-w-full h-screen bg-gradient-to-r from-purple-${currentMode == 'dark' ? 900 : 200} to-indigo-${currentMode == 'dark' ? 800 : 300} flex items-center justify-center`}>
-                    <div className="w-full px-10 text-white ">
+                    <div className={`w-full px-10 text-${currentMode != 'dark' ? 'black' : 'white'}`}>
                         <div className="flex flex-col md:flex-row items-center">
                             <div className="w-full md:w-1/2">
                                 Write page Screen shots here
