15044494a44591b7e9b631111efca3614737de7b
bhavana0415 <bhavanamanaswi@gmail.com>
2024-07-23 15:37:04 +0530

    screen adjustments

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,15 +7,21 @@ import BookDialog from "./BookDialog";
 import { fetchBooks } from "../api/dbBooks/api";
 import { setFavourites } from "../store/Features/favourites/favouritesSlice";
 import { setRecentlyViewed } from "../store/Features/recentlyViewed/recentlyViewedSlice";
+import Snackbar from "@mui/material/Snackbar";
+import { IconButton } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 const Card = ({ book }) => {
   const [rating, setRating] = useState(3);
   const [open, setOpen] = useState(false);
+  const [notificatioOpen, setNotificatioOpen] = useState(false);
   const [bookDetails, setBookDetails] = useState(null);
   const dispatch = useDispatch();
 
   const favourites = useSelector((state) => state.favourites.favourites);
-  const recentlyViewed = useSelector((state) => state.recentlyViewed.recentlyViewed);
+  const recentlyViewed = useSelector(
+    (state) => state.recentlyViewed.recentlyViewed
+  );
 
   const calculateRating = (num) => {
     while (num >= 10) {
@@ -30,9 +36,9 @@ const Card = ({ book }) => {
   };
 
   const handleClickOpen = async () => {
-    if(recentlyViewed.findIndex((bk)=>book.id==bk.id)==-1){
-      const recentViews = [...recentlyViewed,book];
-      dispatch(setRecentlyViewed(recentViews))
+    if (recentlyViewed.findIndex((bk) => book.id == bk.id) == -1) {
+      const recentViews = [...recentlyViewed, book];
+      dispatch(setRecentlyViewed(recentViews));
     }
     try {
       dispatch(setIsLoading(true));
@@ -41,9 +47,11 @@ const Card = ({ book }) => {
         console.log(response);
         setBookDetails(response);
         setOpen(true);
+      } else {
+        setNotificatioOpen(true);
       }
     } catch (error) {
-      console.log(error);
+      console.log("error", error);
     } finally {
       dispatch(setIsLoading(false));
     }
@@ -71,8 +79,22 @@ const Card = ({ book }) => {
     setRating(calculateRating(Number(book.id.replace(/\D/g, ""))));
   }, [book.id]);
 
+  const action = (
+    <>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={()=>setNotificatioOpen(false)}>
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  );
+
   return (
-    <div key={book.id} className="justify-center items-center shadow-sm shadow-cyan-500 p-4 rounded-lg h-full">
+    <div
+      key={book.id}
+      className="justify-center items-center shadow-sm shadow-cyan-500 p-4 rounded-lg h-full">
       <div className={`relative rounded-xl flex flex-col h-full`}>
         <button onClick={handleClickOpen}>
           <div className="relative transform transition duration-500 hover:scale-105 flex items-center justify-center overflow-hidden rounded-xl bg-fuchsia-300 h-64">
@@ -89,31 +111,31 @@ const Card = ({ book }) => {
             Array(rating)
               .fill(0)
               .map((_, ind) => (
-                  <svg
-                    key={ind}
-                    className="block h-3 w-3 align-middle text-yellow-500 sm:h-4 sm:w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor">
-                    <path
-                      d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                      className=""></path>
-                  </svg>
+                <svg
+                  key={ind}
+                  className="block h-3 w-3 align-middle text-yellow-500 sm:h-4 sm:w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                    className=""></path>
+                </svg>
               ))}
           {rating < 5 &&
             Array(5 - rating)
               .fill(0)
               .map((_, ind) => (
-                  <svg
-                    key={ind}
-                    className="block h-3 w-3 align-middle text-gray-400 sm:h-4 sm:w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor">
-                    <path
-                      d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                      className=""></path>
-                  </svg>
+                <svg
+                  key={ind}
+                  className="block h-3 w-3 align-middle text-gray-400 sm:h-4 sm:w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                    className=""></path>
+                </svg>
               ))}
 
           {checkIfFav() ? (
@@ -132,7 +154,16 @@ const Card = ({ book }) => {
         </div>
       </div>
       {open && (
-        <BookDialog open={open} handleClose={handleClose} book={bookDetails} />  
+        <BookDialog open={open} handleClose={handleClose} book={bookDetails} />
+      )}
+      {notificatioOpen && (
+        <Snackbar
+          open={notificatioOpen}
+          autoHideDuration={3000}
+          onClose={()=>setNotificatioOpen(false)}
+          message="Unable to fetch Book"
+          action={action}
+        />
       )}
     </div>
   );
